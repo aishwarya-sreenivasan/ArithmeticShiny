778a4d6283d2291ed525779f1bf022ae481dacd4
aishwarya-sreenivasan <aishwaryas865@gmail.com>
2025-09-30 10:25:50 -0500

    push shiny

--- app.R
+++ app.R
@@ -1,31 +1,45 @@
+
 library(shiny)
 
 ui <- fluidPage(
-    titlePanel("Let's Do Arithmetic!"),
-    sidebarLayout(
-        sidebarPanel(
-            numericInput(inputId = "number1",
-                         label = "Number 1",
-                         value = 0),
-            numericInput(inputId = "number2",
-                         label = "Number 2",
-                         value = 0)
-        ),
-        mainPanel(
-            h2("The sum of the two numbers is:"),
-            textOutput("sum")
-        )
+  titlePanel("Let's Do Arithmetic!"),
+  sidebarLayout(
+    sidebarPanel(
+      numericInput(inputId = "number1",
+                   label = "Number 1",
+                   value = 0),
+      numericInput(inputId = "number2",
+                   label = "Number 2",
+                   value = 0),
+      actionButton("btn_add", "Add"),
+      actionButton("btn_mul", "Multiply")
+    ),
+    mainPanel(
+      verbatimTextOutput("result")
     )
+  )
 )
 
-server <- function(input, output) {
-    output$sum <- renderText({
-        x1 <- as.numeric(input$number1)
-        x2 <- as.numeric(input$number2)
-        x1 + x2
-    })
+server <- function(input, output, session) {
+    result_val <- reactiveVal(NULL)
+    observeEvent(input$btn_add, {
+    res <- input$number1 + input$number2
+    result_val(res)
+  })
+    observeEvent(input$btn_mul, {
+    res <- input$number1 * input$number2
+    result_val(res)
+  })
+  output$result <- renderText({
+    rv <- result_val()
+    if (is.null(rv)) {
+      "Select a function"
+    } else {
+      paste("Result:", rv)
+    }
+  })
 }
 
-shinyApp(ui = ui, server = server)
+shinyApp(ui, server)
 
 
